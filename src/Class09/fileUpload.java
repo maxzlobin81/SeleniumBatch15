15865f3cea1b21241195e86dc5c41a0b54b859fc
maxzlobin81 <maxzlobin@gmail.com>
2023-03-29 13:05:32 -0500

    3.29.23 commit

---
+++ src/Class09/fileUpload.java
@@ -0,0 +1,26 @@
+package Class09;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class fileUpload {
+    public static void main(String[] args) {
+
+//        tell your project where the webdriver is located.
+//        for mac user please do not use .exe with chromedriver
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+
+        //        create an instance of WebDriver
+        WebDriver driver = new ChromeDriver();
+//        max the window
+        driver.manage().window().maximize();
+
+        driver.get("https://the-internet.herokuapp.com/upload");
+
+        WebElement chooseFile = driver.findElement(By.xpath("//input[@id='file-upload']"));
+        chooseFile.sendKeys("C:\\Users\\moazzam\\Desktop\\hello.txt");
+
+    }
+}
---
+++ src/Class09/screenshot.java
@@ -0,0 +1,44 @@
+package Class09;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.IOException;
+
+public class screenshot {
+    public static void main(String[] args) throws IOException {
+
+//        tell your project where the webdriver is located.
+//        for mac user please do not use .exe with chromedriver
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+
+        //        create an instance of WebDriver
+        WebDriver driver = new ChromeDriver();
+//        max the window
+        driver.manage().window().maximize();
+
+        driver.get("http://secure.smartbearsoftware.com/samples/testcomplete11/WebOrders/login.aspx");
+
+//        username Text Box
+        WebElement userName = driver.findElement(By.xpath("//input[@name='ctl00$MainContent$username']"));
+        userName.sendKeys("Tester");
+
+//        password field
+        WebElement pass = driver.findElement(By.xpath("//input[@name='ctl00$MainContent$password']"));
+        pass.sendKeys("test");
+        //        password field
+
+//        take screenshot
+        TakesScreenshot ss = (TakesScreenshot) driver;
+//        take the scrren shot as file
+
+        File sourceFile = ss.getScreenshotAs(OutputType.FILE);
+
+//        save the file in computer
+        FileUtils.copyFile(sourceFile,new File("/Users/maximrealtypros/IdeaProjects/SeleniumBatch15/src/screenshot/abc.png"));
+
+
+    }
+}
